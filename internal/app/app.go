cfc6fbbba3ed24ebb02f09b2ddb06de089b39204
dart-chain <timdseng@gmail.com>
2025-09-06 12:24:37 +0300

    init an app and run function

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,13 +1,27 @@
 package app
 
+import "gitlab.com/tim_de/dartlog/v2/dartlog"
+
 type App struct {
-	Name string
+	Name    string
+	Version string
+
+	Logger *dartlog.Logger
 }
 
-func NewApp(name string) *App {
-	return &App{Name: name}
+func New(name, version string, logger *dartlog.Logger) *App {
+	return &App{
+		Name:    name,
+		Version: version,
+		Logger:  logger,
+	}
 }
 
-func (a *App) Run() string {
-	return "App " + a.Name + " is running!"
+func (a *App) Run() error {
+	a.Logger.Log(dartlog.INFO, "App", "Application is running", map[string]any{
+		"name":    a.Name,
+		"version": a.Version,
+	})
+	// Application logic goes here
+	return nil
 }
