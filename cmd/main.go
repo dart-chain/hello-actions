5c39fde4efcb3cb16a60e2914fe602adc3909501
dart-chain <timdseng@gmail.com>
2025-09-06 12:22:42 +0300

    added a version control and app instance to run an application

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"github.com/dart-chain/hello-actions/internal/app"
+	"github.com/dart-chain/hello-actions/internal/utils"
+	"gitlab.com/tim_de/dartlog/v2/dartlog"
+)
+
+var (
+	version string
+)
+
 func main() {
-	println("Hello, World!")
+	logger := dartlog.New("hello-actions", true, true)
+
+	logger.Log(dartlog.INFO, "main", "Starting application...", nil)
+
+	if !utils.IsVersionValid(version) {
+		logger.Log(dartlog.FATAL, "main", "Invalid version format", map[string]any{"version": version})
+	}
+
+	app := app.New("hello-actions", utils.ExtractVersion(version), logger)
+
+	if err := app.Run(); err != nil {
+		logger.Log(dartlog.FATAL, "main", "Application error", map[string]any{"error": err.Error()})
+	}
+	logger.Log(dartlog.INFO, "main", "Application finished successfully", nil)
+
 }
